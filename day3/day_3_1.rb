f2a833204845e687d81e9c414bc33b866306096a
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2023-12-03 19:23:21 +0000

    Refactor day 3 part 1

--- day3/day_3_1.rb
+++ day3/day_3_1.rb
@@ -6,22 +6,20 @@ def has_adjacent?(matrix, positions, i, j)
   positions.any? do |position|
     x = i + position[0]
     y = j + position[1]
-    #puts "x: #{x}, y: #{y}"
-    #puts matrix[x][y]
     matrix[x][y] =~ /[^.\w]/
   end
 end
 
-
-
-# @param matrix [Array]
+# @param matrix [Array<Array<String>>]
 def get_numbers(matrix)
   sum = 0
   matrix.each_with_index do |line, i|
-    end_of_line = line.length - 2
+    end_of_line = line.length - 1
     num = ''
     has_adjacent = false
-    puts "line ##{i+1}"
+
+    next unless line.join('').match? /[^.]/
+
     line.each_with_index do |element, j|
       if element =~ /\d/
         num << element
@@ -38,49 +36,7 @@ def get_numbers(matrix)
 
       next if element == '.'
 
-      positions = []
-
-      is_first_line = i == 0
-      is_middle_col = j != 0 && j < end_of_line
-      is_first_col = j == 0
-
-      if is_first_line
-        puts "first line"
-        if is_middle_col
-          puts "middle of first line"
-          positions = [[0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-        else
-          if is_first_col
-            puts "first row first line"
-            positions = [[0, 1], [1, 0], [1, 1]]
-          else
-            puts "last row first line"
-            positions = [[0, -1], [1, -1], [1, 0]]
-          end
-        end
-      end
-
-      is_last_line = i == matrix.length - 1
-
-      if is_last_line
-        if is_middle_col
-          positions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1]]
-        else
-          if is_first_col
-            positions = [[-1, 0], [-1, 1], [0, 1]]
-          else
-            positions = [[-1, -1], [-1, 0], [0, -1]]
-          end
-        end
-      end
-
-      is_middle_line = !is_first_line && !is_last_line
-
-      # middle
-      if is_middle_line && is_middle_col
-        positions = [[-1 ,-1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-      end
-
+      positions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
       has_adjacent = has_adjacent?(matrix, positions, i, j)
     end
   end
@@ -88,9 +44,15 @@ def get_numbers(matrix)
   sum
 end
 
-
 lines = File.readlines('input-3.txt')
+size = lines[0].size
 
-matrix = lines.map { |line| line.split('') }
+# add first empty row
+matrix = [Array.new(size + 2) { |i| '.' }]
+
+matrix += lines.map { |line| ['.'] + line.strip.split('') + ['.'] }
+
+# add last empty row
+matrix += [Array.new(size + 2) { |i| '.' }]
 
 puts get_numbers matrix
